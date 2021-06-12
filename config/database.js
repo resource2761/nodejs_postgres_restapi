05f0b4dfe76ff368de6f2bf8db5757458a811281
resource2761 <resource@emercees.com>
2021-06-12 19:08:12 +0530

    update at 12-06-2021 7:08pm

--- config/database.js
+++ config/database.js
@@ -8,11 +8,18 @@ const pool = new Pool(
         // database: process.env.db_database,
         // connectionLimit: process.env.db_connectionLimit
 
+        // port: 5432,
+        // host: 'ec2-54-91-188-254.compute-1.amazonaws.com',
+        // user: 'gexqfvioyuwjmi',
+        // password: 'f9c47b9a046ca5876bddd8d16b0b4745dc4d55e3030b0baa232d0b35886acc78',
+        // database: 'dcjaks1eo81bnq',
+        // connectionLimit: 10
+
         port: 5432,
-        host: 'ec2-54-91-188-254.compute-1.amazonaws.com',
-        user: 'gexqfvioyuwjmi',
-        password: 'f9c47b9a046ca5876bddd8d16b0b4745dc4d55e3030b0baa232d0b35886acc78',
-        database: 'dcjaks1eo81bnq',
+        host: 'database-2.cgjeatbud40v.us-east-2.rds.amazonaws.com',
+        user: 'postgres',
+        password: 'manish123manish',
+        database: 'awspostgres',
         connectionLimit: 10
 
     }
