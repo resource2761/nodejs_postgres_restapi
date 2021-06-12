ad8c22ed4ea5b0b74cbd2329fb715eef4fdb52d9
resource2761 <resource@emercees.com>
2021-06-12 14:49:50 +0530

    db again update 12-06-2021

--- app.js
+++ app.js
@@ -13,8 +13,17 @@ app.use(express.json());
 // define the router begin path as /customer/data
 app.use("/customer/data",userrouter);
  
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 // use .env from process.env
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`server is running on port ${PORT}`);
+// })
+
+
+
+const PORT = process.env.PORT || 80;
+var server = app.listen(PORT, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("server is listening at http://%s:%s", host, port);
+});
